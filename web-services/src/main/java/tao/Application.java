2ec49314daa4dd63d71bc5eeefce0b0fb9c8ee8a
ZHENG Tao <nicolas2lee@gmail.com>
2018-06-22 16:45:31 +0200

    [back] add spring boot main entry point

--- web-services/src/main/java/tao/Application.java
+++ web-services/src/main/java/tao/Application.java
@@ -1,8 +1,19 @@
 package tao;
 
-//@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        /**
+         * To avoid class loader memory leak, we should close spring context when undeploy or close the application,
+         * registerShutdownHook is useful only when the application is deployed into jvm directly: like docker
+         * for traditional deployment onto servers: tomcat, weblogic
+         * should check this issue: https://github.com/spring-cloud/spring-cloud-commons/issues/211
+         */
+        applicationContext.registerShutdownHook();
     }
 }
