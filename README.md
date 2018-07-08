8ab65c30f69d06c7934724c4e1284ee13764c354
ZHENG Tao <nicolas2lee@gmail.com>
2018-07-08 21:44:17 +0200

    [test] add cucumber report

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Install maven wrapper
 Use provider instead of inject directly for bean with scope prototype
 
 Perfer @Resource than @Qualifier for filtering bean by name
+
+Use atomikos with spring boot for transcation manangement (like: two phase commit)
--- application/src/test/java/com/bnpparibas/dsibddf/nomenclature/application/usecase/core/e2e/CucumberTest.java
+++ application/src/test/java/com/bnpparibas/dsibddf/nomenclature/application/usecase/core/e2e/CucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "com.bnpparibas.dsibddf.nomenclature",
+        glue = {"com.bnpparibas.dsibddf.nomenclature.application"},
         features = { "classpath:features/nomencalture.feature" },
         plugin ={"pretty",
                 "html:target/cucumber-report/",
